9c5bddc6d1cca69f14b0b575de7064808a5ea4bb
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2025-10-24 11:17:19 +0300

    changed types from id to node_it_t

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -10,14 +10,14 @@ Graph::Graph(size_t graph_size, size_t num_edges, size_t thread_pool_size) : thr
     for(auto i : std::views::iota(size_t{0}, graph_size))
         AddNode();
 
-    size_t max_num_edges = graph_size*(graph_size - 1)/2;
-    if(num_edges > max_num_edges)
-        num_edges = max_num_edges;
+    size_t num_max_edges = graph_size*(graph_size - 1)/2;
+    if(num_edges > num_max_edges)
+        num_edges = num_max_edges;
 
     std::vector<std::pair<node_id_t, node_id_t>> all_edges;
-    all_edges.reserve(graph_size*(graph_size-1)/2);
-    for (int i = 0; i < graph_size; ++i)
-        for (int j = i + 1; j < graph_size; ++j)
+    all_edges.reserve(num_max_edges);
+    for (node_id_t i = 0; i < graph_size; ++i)
+        for (node_id_t j = i + 1; j < graph_size; ++j)
             all_edges.emplace_back(i, j);
 
     std::vector<std::pair<node_id_t, node_id_t>> chosen;
@@ -25,6 +25,7 @@ Graph::Graph(size_t graph_size, size_t num_edges, size_t thread_pool_size) : thr
     std::sample(all_edges.begin(), all_edges.end(),
                 std::back_inserter(chosen),
                 num_edges, std::mt19937{std::random_device{}()});
+
 
     for (auto [i, j] : chosen)
         AddEdge(i, j);
