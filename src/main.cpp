03a9d943c6bf0fb3002b09720f2ca595a638a085
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-05-08 09:36:32 +0000

    add a tracy call to make sure it connects

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "MISDistributedRouting/utils/log_macros.h"
 #include "MISDistributedRouting/simulation/simulator.h"
+#include <tracy/Tracy.hpp>
 
 int main()
 {
     INIT_LOGGER();
-    
+    ZoneScoped;
     Simulator sim;
 
     sim.Run();
