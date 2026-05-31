375134c8db86794fc1caba264ceb0f655ba057f9
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-05-31 21:41:19 +0000

    add benchmarking

---
+++ benchmarks/bench_main.cpp
@@ -0,0 +1,30 @@
+#include <benchmark/benchmark.h>
+#include "MISDistributedRouting/simulation/simulation_graph.h"
+#include "MISDistributedRouting/utils/log_macros.h"
+
+static void BM_MIS_building(benchmark::State& state){
+    INIT_LOGGER();
+
+    SimulationGraph graph(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
+    
+
+    for(auto _ : state){
+        state.PauseTiming();
+        graph.InitGraph(state.range(0));
+        state.ResumeTiming();
+
+        graph.AdvanceStatus();
+        while(graph.GetGraphStage() != MIS_Node::MIS_Stage::COMPLETE){
+            graph.RunCycle();
+        }
+
+        benchmark::DoNotOptimize(graph);
+        benchmark::ClobberMemory();
+    }
+    
+}
+
+
+BENCHMARK(BM_MIS_building)->Arg(10)->Arg(100)->Arg(1000);
+
+BENCHMARK_MAIN();
