d59c83fb8405d8cc54b0ade6f690dd9a70924d44
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-06-01 19:53:30 +0000

    change message box to reduce the number of allocations for messages instead use existing allocated memory

--- include/MISDistributedRouting/core/message_box.h
+++ include/MISDistributedRouting/core/message_box.h
@@ -7,8 +7,22 @@
 #include "message.h"
 
 // TODO consider a bitset per buffer that marks where messages were accepted - potentially faster to check if empty
+// TODO consider using a vector without clearing its memory - just saving indexes of start and end
+struct NeighborInbox{
+    size_t size;
+    size_t front_msg_index;
+    std::vector<Message> inbox;
+
+    NeighborInbox();
+    void AddMsg(Message&& msg);
+    [[nodiscard]] Message PopMsg();
+    [[nodiscard]] bool Empty() const;
+    void Clear();
+};
+
+
 struct Inbox{
-    std::array<std::vector<std::vector<Message>>, 2> buffers{};
+    std::array<std::vector<NeighborInbox>, 2> buffers{};
 
     int read_buffer_index{0};
     int write_buffer_index{1};
